bee802a4ea4abf7ed19bf9866e9fa0223db59f56
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-06-01 07:54:50 +0530

    Contact Me section completed

---
+++ script.js
@@ -0,0 +1,26 @@
+window.addEventListener('load',()=>{
+    let allTab = document.querySelectorAll('.tab');
+    let allproject = document.querySelectorAll('.project');
+    const removetab = ()=>{
+        allTab.forEach(tab =>{
+            tab.classList.remove('tab-active')
+        })
+    }
+    allTab.forEach(tab =>{
+        tab.addEventListener('click',()=>{
+            removetab()
+            tab.classList.add('tab-active');
+            let filterName = event.target.getAttribute('data-name');
+            allproject.forEach(project =>{
+                let projectName = project.getAttribute('data-name');
+                if((projectName == filterName)|| filterName == 'All'){
+                    project.style.display  = 'block';
+                }
+                else{
+                    project.style.display = 'none';
+                }
+        })
+   
+    })
+    })
+})
